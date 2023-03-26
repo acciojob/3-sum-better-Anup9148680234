ab90269e87511a77238c655c90815a6bbf043e78
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-03-26 19:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function threeSum(arr, target) {
+function threeSum(S, target) {
 // write your code here
-  
+  const n = S.length;
+  S.sort((a, b) => a - b);
+  let diff = Infinity;
+  let closestSum = 0;
+  for (let i = 0; i < n - 2; i++) {
+    let j = i + 1;
+    let k = n - 1;
+    while (j < k) {
+      const tripletSum = S[i] + S[j] + S[k];
+      const currentDiff = Math.abs(tripletSum - target);
+      if (currentDiff < diff) {
+        diff = currentDiff;
+        closestSum = tripletSum;
+      }
+      if (tripletSum < target) {
+        j++;
+      } else if (tripletSum > target) {
+        k--;
+      } else {
+        return tripletSum;
+      }
+    }
+  }
+  return closestSum;
 }
 
 module.exports = threeSum;
